8854f5174a86de9cc8fd4a56fc0bf5b3d748e92b
Maxwell Fang <maxwellfang63@gmail.com>
2026-08-22 22:30:49 -0400

    Untracking Jupyter notebook files

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from pose_estimation import load_model, train_HanCo
 if __name__ == '__main__':
-    model = load_model('./pose_estimation/models/12.pt')
-    train_HanCo(batch_size=32, epochs=88, model=model, start_epoch=12, learning_rate=0.001, load_weights=False, patience=20)
+    model = load_model('./pose_estimation/models/79.pt')
+    train_HanCo(batch_size=32, epochs=100, model=model, start_epoch=100, learning_rate=0.0001, load_weights=False, patience=20)
